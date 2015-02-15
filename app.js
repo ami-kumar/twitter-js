8820910daf192b4a868fd27ecadb0c89167f958b
Sandya Sankarram <ss4119@columbia.edu>
2015-02-15 18:15:08 -0500

    prepopulate name

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Main application 
 var express = require( 'express' )
 var morgan = require('morgan')
 var app = express()
@@ -8,22 +7,12 @@ var bodyParser = require('body-parser');
 
 app.use( morgan( 'dev' ) )
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', routes);
 
 
-// parse application/json
 app.use(bodyParser.json())
-
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
-
-
 
 app.use(express.static(__dirname + '/public'));
 
@@ -34,7 +23,7 @@ app.set('views', __dirname + "/views")
 
 swig.setDefaults({cache : false});
 
-var server = app.listen(5210, function() {
+var server = app.listen(5344, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log( 'Server listening at http:%s:%s', host, port )
--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,31 @@ var tweetBank = require( '../tweetBank' )
 
 router.get( '/', function( req, res ) {
 	var tweets = tweetBank.list()
-	res.render( 'index', {title: 'Twitter.js', tweets: tweets, showForm: true });
+	res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
 })
 
-router.get( '/users/:name', function( req, res ) {
-	var name = req.params.name
-	var tweets = tweetBank.find( {name: name} )
-	res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: tweets, showForm: true} )
-} )
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  console.log(list);
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name , tweets: list, showForm: true, user: name} );
+});
 
-router.get( '/users/:name/tweets/:id', function( req, res ) {
-	var name = req.params.name
-	var id = req.params.id
-	var tweets = tweetBank.find( {name: name, id: id} )
-	res.render( 'index', { title: 'Twitter.js - Post by ' + name, tweets: tweets, showForm : false } )
-} )
-
-//showForm: true
+router.get('/users/:name/tweets/:id', function(req, res) {
+  var id = req.params.id;
+  var name = req.params.name;
+  var list = tweetBank.find( {id: id} );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+ name , tweets: list, showForm: true, user: name } );
+});
 
 router.post('/submit', function(req, res) {
-	console.log(req);
+  JSON.stringify(req.body, null, 2);
   var name = req.body.name;
   var text = req.body.text;
   tweetBank.add(name, text);
-
-  	res.redirect('/');
+  res.redirect('/');
 });
+
 
 
 module.exports = router
