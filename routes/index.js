05b862432b6153ade81f97b521a7ba7dd98bfe61
ami-kumar <ak3284@columbia.edu>
2015-02-15 16:18:07 -0500

    added index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
-var express = require('express');
-var router = express.Router();
-// could use one line instead: var router = require('express').Router();
-var tweetBank = require('../tweetBank');
+var express = require( 'express')
+var router = express.Router()
+var tweetBank = require( '../tweetBank' )
 
-router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
-});
+router.get( '/', function( req, res ) {
+	var tweets = tweetBank.list()
+	res.render( 'index', { title: 'Twitter.js', tweets: tweets } )
+})
 
-module.exports = router;
+router.get( '/users/:name', function( req, res ) {
+	var name = req.params.name
+	var tweets = tweetBank.find( {name: name} )
+	res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: tweets } )
+} )
+
+module.exports = router
