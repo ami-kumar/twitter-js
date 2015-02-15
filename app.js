b57abae54296bdb0a903712725a3d9e46ffc319f
Sandya Sankarram <ss4119@columbia.edu>
2015-02-15 17:30:30 -0500

    posting

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ var morgan = require('morgan')
 var app = express()
 var swig = require('swig')
 var routes = require('./routes/');
-
+var bodyParser = require('body-parser');
 
 app.use( morgan( 'dev' ) )
 app.use('/', routes);
 
 app.use(express.static(__dirname + '/public'));
+
+app.use( bodyParser.urlencoded( { extended: false } ) )
+app.use( bodyParser.json() )
+app.use(function( req, res ) {
+  res.setHeader( 'Content-Type', "text/plain" )
+  res.write( 'you posted: \n' )
+  res.end( JSON.stringify( req.body, null, 2 ) )
+} )
 
 app.engine('html', swig.renderFile)
 app.set('view engine', "html")
@@ -17,9 +25,7 @@ app.set('views', __dirname + "/views")
 
 swig.setDefaults({cache : false});
 
-
-
-var server = app.listen( 3000, function() {
+var server = app.listen(4225, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log( 'Server listening at http:%s:%s', host, port )
--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,30 @@ var tweetBank = require( '../tweetBank' )
 
 router.get( '/', function( req, res ) {
 	var tweets = tweetBank.list()
-	res.render( 'index', { title: 'Twitter.js', tweets: tweets } )
+	res.render( 'index', {title: 'Twitter.js', tweets: tweets, showForm: true });
 })
 
 router.get( '/users/:name', function( req, res ) {
 	var name = req.params.name
 	var tweets = tweetBank.find( {name: name} )
-	res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: tweets } )
+	res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: tweets, showForm : false } )
 } )
 
 router.get( '/users/:name/tweets/:id', function( req, res ) {
 	var name = req.params.name
 	var id = req.params.id
 	var tweets = tweetBank.find( {name: name, id: id} )
-	res.render( 'index', { title: 'Twitter.js - Post by ' + name, tweets: tweets } )
+	res.render( 'index', { title: 'Twitter.js - Post by ' + name, tweets: tweets, showForm : false } )
 } )
+
+//showForm: true
+
+router.post('/submit', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
+
 
 module.exports = router
