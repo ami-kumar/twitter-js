ccf0860270b111509f6d30cb2b302669bc039bf6
Sandya Sankarram <ss4119@columbia.edu>
2015-02-15 17:34:09 -0500

    fixing previous mistake

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('views', __dirname + "/views")
 
 swig.setDefaults({cache : false});
 
-var server = app.listen(4225, function() {
+var server = app.listen(4441, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log( 'Server listening at http:%s:%s', host, port )
